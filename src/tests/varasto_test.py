a4addcff5811416e2d86cd3a43a9033794f783c1
Natalie Johansson <mnrjohansson@gmail.com>
2021-11-04 15:14:51 +0200

    testit 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,36 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_muuta_ei_mahdu_jos_lisataan_liikaa(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(),0)
+    
+    def test_negatiivinen_tilavuus_nollataan(self):
+        varasto_b = Varasto(-20)
+        self.assertAlmostEqual(varasto_b.tilavuus, 0)
+    
+    def test_negatiivinen_saldo_nollataan(self):
+        varasto_b = Varasto(10, -20)
+        self.assertAlmostEqual(varasto_b.saldo, 0)
+    
+    def test_jos_saldo_on_suurempi_kuin_tilavuus_varasto_tayteen(self):
+        varasto_b = Varasto(10, 20)
+        self.assertAlmostEqual(varasto_b.paljonko_mahtuu(), 0)
+    
+    def test_saldo_ei_muutu_jos_lisattava_maara_on_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-10)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_saldo_ei_muutu_jos_otetaan_negatiivinen_maara(self):
+        self.varasto.ota_varastosta(-10)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_saldo_nollataa_jos_yritetaan_ottaa_enemmän_kuin_voidaan(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.ota_varastosta(20)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_eri_varastoilla_on_eri_string(self):
+        varasto_b = Varasto(20, 5)
+        self.assertNotEqual(str(self.varasto), str(varasto_b))
